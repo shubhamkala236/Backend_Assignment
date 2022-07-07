81c9d108c3549c922953f88ded4b0ae92ef428dd
shubham <lawkktd@gmail.com>
2022-07-07 13:13:25 +0530

    new

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,19 +1,29 @@
 import express from "express";
 import { deleteUser,getUser,updateUser } from '../controllers/user.js';
+import {verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 
 
 const router = express.Router();
 
+router.get("/checkauthentication",verifyToken,(req,res,next)=>{
+    res.send("hello user you are logged in")
+})
+
+router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
+    res.send("hello user you are logged in and can delete your account")
+})
+
+
 //UPDATE 
-router.put("/:id", updateUser);
+router.put("/:id",verifyUser, updateUser);
 
 //DeleteUser
 
-router.delete("/:id", deleteUser);
+router.delete("/:id",verifyUser, deleteUser);
 
 //GET
-router.get("/:id", getUser);
+router.get("/:id",verifyUser, getUser);
 
 
 export default router;
